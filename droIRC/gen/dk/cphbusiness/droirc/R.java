c87eae10e70e92644eec2fcb285107e8f83f3004
mhck <heckmanator@gmail.com>
2013-11-10 14:08:43 +0100

    Added menu

--- droIRC/gen/dk/cphbusiness/droirc/R.java
+++ droIRC/gen/dk/cphbusiness/droirc/R.java
@@ -22,10 +22,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int menupic=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int action_settings=0x7f090005;
         public static final int editText1=0x7f090002;
+        public static final int imageView1=0x7f090003;
+        public static final int listView1=0x7f090004;
         public static final int scrollView1=0x7f090000;
         public static final int textView1=0x7f090001;
     }
@@ -36,10 +39,12 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
+        public static final int activity_menu=0x7f030001;
     }
     public static final class menu {
         public static final int chat=0x7f080000;
-        public static final int network=0x7f080001;
+        public static final int menu=0x7f080001;
+        public static final int network=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
@@ -47,6 +52,9 @@ public final class R {
         public static final int chatmsgshere=0x7f060003;
         public static final int entermsghere=0x7f060004;
         public static final int hello_world=0x7f060002;
+        public static final int menu_pic=0x7f060007;
+        public static final int nickname_label=0x7f060008;
+        public static final int title_activity_menu=0x7f060006;
         public static final int title_activity_network=0x7f060005;
     }
     public static final class style {
